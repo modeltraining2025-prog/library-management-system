16ca68a45978826107bd675d0e520217eb1a9389
hgfkhjfkj <modeltraining2025@gmail.com>
2026-04-21 15:58:22 +0530

    DEMO

---
+++ 9.py
@@ -0,0 +1,10 @@
+def diction(strr):
+    freq = {}
+    for i in strr.split():
+        ch = i[0]
+        if ch not in freq:  freq[ch] = []
+        freq[ch].append(i)
+    return freq
+
+strr = 'hello buddy how are you'
+print(diction(strr))
